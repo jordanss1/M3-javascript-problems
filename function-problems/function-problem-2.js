ddd9618bc4f149d800bf3e558488110ddeac2dbe
jordanss1 <jordan.shade@live.com>
2022-04-15 20:32:23 +0100

    final commit for function problem 2; fixed syntax error and added if else to prohibit number use in function

--- function-problems/function-problem-2.js
+++ function-problems/function-problem-2.js
@@ -3,23 +3,32 @@
 /*
 1. Write the first line of the function
 
+2. Create an if else statement that would filter whether the user has entered a number or string
+
 2. Executes .split on the string to extract every individual word into an array as it's own element, initialized in the "words" array
 
-3. Execute .reduce on the array to compare each string to see which one is the longest and return that string to the function
+3. Execute .reduce on the array to compare each string to see which one is the longest and return that string to the function and a few extra "else" conditions in case there are equal lengths.
 
-4.  
+4.  And a final return which returns the longest word to the function
  */
 
 const longestWord = string => {
+  if (typeof string === "number") {
+	 return console.log("No numbers allowed!");
+  } else {
+	 console.log("String magic: begin");
+  }
   let words = string.split(" ");
   let longest = words.reduce((stringA, stringB) => {
      if (stringA.length > stringB.length) {
 		 return stringA;
+	 } else if (stringA.length === stringB.length) {
+		 return [stringA, stringB];
 	 } else {
 		 return stringB;
 	 }
-  )};
-	return console.log(longest);
+  });
+	return longest;
 };
 
-console.log(longestWord("The mighty mouse flew on an airplane and drove to Massachusetts"));
+console.log(longestWord(1111111));
