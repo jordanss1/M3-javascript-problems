8bca602d05095ad6b7640372288a1cd5929bf4af
jordanss1 <jordan.shade@live.com>
2022-04-16 15:34:04 +0100

    made comments more neat

--- dom-problems/dom-problem-1.js
+++ dom-problems/dom-problem-1.js
@@ -1,22 +1,20 @@
-// Create a basic HTML page with a P element and a button on it. 
-// Then, write a JavaScript function to set the background color of that 
-//paragraph using an "onclick" event added to a button in the UI.
+// Create a basic HTML page with a P element and a button on it. Then, write 
+//a JavaScript function to set the background color of that paragraph using 
+//an "onclick" event added to a button in the UI.
 
 /*
-1. Initialized variables at the top of script first with the corresponding 
-elements needed for this problem using querySelector
+1. Initialized variables at the top of script first with the corresponding elements needed 
+for this problem using querySelector
 
 2. Since arrow and function expressions must be defined before they are called 
 (unlike function declarations) I defined it before I called it
 
-3. In the function I used the globally scoped "p1" variable which is a 
-reference to the <p> tag in the HTML I needed to use. I appended 
-style to the variable and backgroundColor in order to style css in the code 
-block
+3. In the function I used the globally scoped "p1" variable which is a reference to 
+the <p> tag in the HTML I needed to use. I appended style to the variable and 
+backgroundColor in order to style css in the code block
 
-4. I called an onclick event on the "button1" globally scoped object and
- assigned it with the changeColor function, which styles the ackground 
- red on-click of the button
+4. I called an onclick event on the "button1" globally scoped object and assigned it 
+with the changeColor function, which styles the ackground red on-click of the button
 */
 
 const p1 = document.querySelector("#p1");
