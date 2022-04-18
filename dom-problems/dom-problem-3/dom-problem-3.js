3a9f43340e3451ac0b737fb911c2c328a88cde82
jordanss1 <jordan.shade@live.com>
2022-04-18 21:23:41 +0100

    final commit of day; fixed function for selected index, added comments

--- dom-problems/dom-problem-3/dom-problem-3.js
+++ dom-problems/dom-problem-3/dom-problem-3.js
@@ -1,7 +1,22 @@
+//Create a HTML page with a dropdown list with three 
+//items in it, then write a JavaScript function that 
+//displays the number of items and displays the item 
+//in the list when you select from the dropdown.
+
+/*
+1. Declared "dogNames" variable outside of function as it would need to be used when applying the onchange event globally
+
+2. Created function and initialised a variable assigned to the select tag 
+
+*/
+
+let dogNames = document.querySelector("#dog-names");
+
 const options = () => {
-	let dogNames = document.querySelectorAll("#dog-names");
 	let selected = dogNames.selectedIndex;
+	let p1 = document.querySelector("p");
+	p1.innerText = dogNames.options[selected].text;
 	
 };
 
-options();
+dogNames.onchange = options();
