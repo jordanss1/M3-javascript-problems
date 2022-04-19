5abefff67c5dff0ec1187f9e80760a7b688c894e
jordanss1 <jordan.shade@live.com>
2022-04-19 20:59:28 +0100

    final commit after finishing function to display the selected option text and total number of options in template literal

--- dom-problems/dom-problem-3/dom-problem-3.js
+++ dom-problems/dom-problem-3/dom-problem-3.js
@@ -4,9 +4,19 @@
 //in the list when you select from the dropdown.
 
 /*
-1. Declared "dogNames" variable outside of function as it would need to be used when applying the onchange event globally
+1. Declared "dogNames" variable outside of function as it would need to be used 
+when applying the onchange event globally
 
-2. Created function and initialised a variable assigned to the select tag 
+2. Created function and initialised a variable assigned to which ever selected 
+index of the select tag that is chosen. Initialised the p tag to its own variable.
+
+3. Used .innerText on the <p> to display a template literal in order to display the 
+text of the selected option and the total number of options.
+
+4. Finally attached the onclick event to our select element so everytime an option is 
+chosen in the dropdown, onchange runs the "options" function
+
+
 
 */
 
@@ -15,8 +25,9 @@ let dogNames = document.querySelector("#dog-names");
 const options = () => {
 	let selected = dogNames.selectedIndex;
 	let p1 = document.querySelector("p");
-	p1.innerText = dogNames.options[selected].text;
-	
+	let p2 = document.getElementById("#p2")
+	p1.innerText = `${dogNames.options[selected].text} is the option selected and there 
+	are ${dogNames.length} options total`;
 };
 
-dogNames.onchange = options();
+dogNames.onchange = options;
