8145b3ed16b8c514e8ca9ebe8c0c213e9a6d42e5
jordanss1 <jordan.shade@live.com>
2022-05-04 18:24:15 +0100

    made only one array and one conditional

--- array-problems/array-problem-4/array-problem-4.js
+++ array-problems/array-problem-4/array-problem-4.js
@@ -5,31 +5,28 @@
 1. Initialized a function expression to variable removeDuplicates with a 
 parameter of "arr" for use for passed arrays
 
-2. Initialise two empty arrays "numberArray" and "stringArray". Each will be used to pass each element that is a string or number
+2. Initialise an empty array "finalArray" which will be used to pass each element that is a string or number
 without any duplicate elements
 
-3. Created a for loop to loop every index of the passed array with a nested if else if condition that checks if the 
+3. Created a for loop to loop every index of the passed array with a nested if condition that checks if the 
 current looped element is a string or number
 
-4. Called the "findUniqueElement" function which accepts two parameters, one for the two arrays and one for the 
+4. Called the "findUniqueElement" function which accepts two parameters, one for the array "finalArray" and one for the 
 current looped element. An if condition in the function checks if that item is not in the passed array with the includes 
-method. If it is not included, the following line of code pushes that element into the corresponding passed array
+method. If it is not included the following line of code pushes that element into the passed array "finalArray"
 
-5. This is done for every element of the passed array in "removeDuplicates" until the array is looped, once looped, stringArray and numberArray 
-are merged using the concat method, giving us our array without duplicates.
+5. This is done for every element of the passed array in "removeDuplicates" until the array is looped, once looped, finalArray
+is returned to the function, giving us our array without duplicates.
 */
 
 const removeDuplicates = arr => {
-	let stringArray = [];
-	let numberArray = [];
+	let finalArray = [];
 	for (let i = 0; i < arr.length; i++) {
-		if (typeof arr[i] === "string") {
-			findUniqueElement(stringArray, arr[i]);
-		} else if (typeof arr[i] === "number") {
-			findUniqueElement(numberArray, arr[i]);
-		}
+		if (typeof arr[i] === "string" || "number") {
+			findUniqueElement(finalArray, arr[i]);
+		};
 	}
-	return stringArray.concat(numberArray);
+	return finalArray;
 };
 
 const findUniqueElement = (arrOfItems, item) => {
